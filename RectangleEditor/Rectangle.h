3e99860c14acdff0b2bf3cff39298804fb129630
7j2xgqhj <shimofuwa514@gmail.com>
2020-11-02 15:51:31 +0900

    更新一度目 とりあえずjavaのをうつした

--- RectangleEditor/Rectangle.h
+++ RectangleEditor/Rectangle.h
@@ -1,1 +1,14 @@
 #pragma once
+class Rectangle {
+private:
+	int width;
+	int height;
+	int x;
+	int y;
+	int color;
+public:
+	Rectangle(int width, int height, int x, int y, int color);
+	bool isSegment();
+	bool isPoint();
+	bool isNegative();
+};
