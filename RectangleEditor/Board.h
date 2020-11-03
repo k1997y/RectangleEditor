70cbb1112bef03e68c641c88d1666ed7f13ec1d9
k1997y <k1997y@icloud.com>
2020-11-03 10:17:21 +0900

    onBOardRectをpublicに変更

--- RectangleEditor/Board.h
+++ RectangleEditor/Board.h
@@ -7,12 +7,12 @@ public:
 	Board();
 	~Board();
 
-	std::list<Rectangle> getRectList() { return onBoardRect; }
+	//std::list<Rectangle> getRectList() { return onBoardRect; }
+	//長方形をリストで持つ
+	std::list<Rectangle> onBoardRect;
 private:
 	//TODO: width, height, or length, widthどちらかに統一
 	const int LENGTH = 500;	//縦
 	const int WIDTH = 400;		//横
-
-	//長方形をリストで持つ
-	std::list<Rectangle> onBoardRect;
+	
 };
