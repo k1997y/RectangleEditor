0ab99ecf5c6149c9d40452d89ad4bca55e443636
k1997y <k1997y@icloud.com>
2020-11-03 10:16:39 +0900

    onBoardRectをprivateにする前

--- RectangleEditor/RectangleEditor.cpp
+++ RectangleEditor/RectangleEditor.cpp
@@ -7,20 +7,24 @@ int main(int argc, char **argv) {
 
 	while (!cmd.getIsExit()) {
 		std::cout << "コマンドを入力\n";
-		std::cout << "1: create...新たな長方形を生成\n2: delete...選択した長方形を削除\n3: exit...プログラムの終了\n→";
+		std::cout << "1: create...新たな長方形を生成\n2: delete...選択した長方形を削除\n3: displayBoard...ボード上の長方形を表示\n4: exit...プログラムの終了\n→";
 		std::cin >> cmdNum;
 
 		switch (cmdNum) {
 			case cmd.CREATE:
 				cmd.createCmd();
+				cmd.displayBoardCmd();
 				break;
 			case cmd.DELETE:
 				cmd.deleteCmd();
+				cmd.displayBoardCmd();
 				break;
 			case cmd.DISPLAY:
 				cmd.displayBoardCmd();
+				break;
 			case cmd.EXIT:
 				cmd.exitMsg();
+				break;
 			default:
 				std::cout << "コマンドが不正です．もう一度入力してください．" << std::endl;
 				break;
