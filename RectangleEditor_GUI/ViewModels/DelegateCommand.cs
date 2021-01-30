4bdfa8b3e61d8f599844f60a2df65944854fd6aa
k1997y <k1997y@icloud.com>
2021-01-31 02:05:33 +0900

    DelegateCommandの実装

---
+++ RectangleEditor_GUI/ViewModels/DelegateCommand.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RectangleEditor_GUI.ViewModels {
+	using System.Windows.Input;
+
+	internal class DelegateCommand : ICommand{
+		/// <summary>
+		/// コマンド実行時の処理内容を保持する
+		/// </summary>
+		private Action<Object> _execute;
+
+		/// <summary>
+		/// コマンド実行可能判別の処理内容を保持する
+		/// </summary>
+		private Func<object, bool> _canExecute;
+
+		/// <summary>
+		/// 新しいインスタンスを生成する
+		/// </summary>
+		/// <param name="execute"> コマンド実行処理を指定する </param>
+		public DelegateCommand(Action<object> execute)
+			:this(execute,null) { }
+
+		/// <summary> 
+		/// 新しいインスタンスを生成する
+		/// </summary>
+		/// <param name="execute"> コマンド実行処理を指定する </param>
+		/// <param name="canExecute"> コマンド実行可能判別処理を指定する</param>
+		public DelegateCommand(Action<object> execute, Func<object, bool> canExecute) {
+			this._execute = execute;
+			this._canExecute = canExecute;
+		}
+
+		/// <summary>
+		/// コマンドが実行可能かどうかの判別処理を行う
+		/// </summary>
+		/// <param name="parameter"> 判別処理に対するパラメータを指定する </param>
+		/// <returns> 実行可能な場合にtrueを返す</returns>
+		public bool CanExecute(object parameter) {
+			return (this._canExecute != null) ? this._canExecute(parameter) : true;
+		}
+
+		/// <summary>
+		/// 実行可能かどうかの判別処理に関する状態が変更されたときに発生する
+		/// </summary>
+		public event EventHandler CanExecuteChanged;
+
+		/// <summary>
+		/// CanExecuteChangedイベントを発行する
+		/// </summary>
+		public void RaiseCanExecuteChanged() {
+			var h = this.CanExecuteChanged;
+			if (h != null) h(this, EventArgs.Empty);
+		}
+
+		/// <summary>
+		/// コマンドが実行されたときの処理を行う
+		/// </summary>
+		/// <param name="parameter">コマンドに対するぱためーたを指定する</param>
+		public void Execute(object parameter) {
+			if(this._execute != null) {
+				this._execute(parameter);
+			}
+		}
+	}
+}
--- RectangleEditor_GUI/ViewModels/MainViewModel.cs
+++ RectangleEditor_GUI/ViewModels/MainViewModel.cs
@@ -3,6 +3,37 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace RectangleEditor_GUI.ViewModels {
-	class MainViewModel {
+	using System.ComponentModel;
+	using System.Runtime.CompilerServices;
+
+	/// <summary>
+	/// MainViewウィンドウに対するデータコンテキストを表す
+	/// </summary>
+	internal class MainViewModel : NotificationObject {
+		private string _upperString;
+		/// <summary>
+		/// 全て大文字に変換した文字列を取得する
+		/// </summary>
+		public string UpperString {
+			get { return this._upperString; }
+			private set { SetProperty(ref this._upperString, value); }
+		}
+
+		private string _inputString;
+		/// <summary>
+		/// 入力文字列を取得または設定する
+		/// </summary>
+		public string InputString {
+			get { return this._inputString; }
+			set {
+				if (SetProperty(ref this._inputString, value)) {
+					//入力文字列を大文字に変換
+					this.UpperString = this._inputString.ToUpper();
+
+					//出力ウィンドウに結果表示
+					System.Diagnostics.Debug.WriteLine(this._inputString);
+				}
+			}
+		}
 	}
 }
